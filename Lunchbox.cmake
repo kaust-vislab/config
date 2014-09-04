7448096692d26a0700f54770671c998f787f2571
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-09-04 13:52:17 +0200

    Add optional SKV dependency to Lunchbox

--- Lunchbox.cmake
+++ Lunchbox.cmake
@@ -1,9 +1,8 @@
 
 set(LUNCHBOX_PACKAGE_VERSION 1.9)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
-set(LUNCHBOX_REPO_TAG master)
 set(LUNCHBOX_DEPENDS eyescale eyescalePorts hwloc DNSSD LibJpegTurbo OpenMP MPI
-                     leveldb REQUIRED Boost)
+                     leveldb skv REQUIRED Boost)
 set(LUNCHBOX_DEB_DEPENDS libboost-regex-dev libboost-serialization-dev
   libhwloc-dev libavahi-compat-libdnssd-dev libboost-filesystem-dev
   libboost-system-dev libboost-thread-dev
